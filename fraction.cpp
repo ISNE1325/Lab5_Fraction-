9e85d009392318e92d658d2dec0b62305562dc64
Jidaphaag <Jidapha_su@cmu.ac.th>
2026-01-07 02:17:02 +0700

    .

--- fraction.cpp
+++ fraction.cpp
@@ -21,11 +21,27 @@ class fraction{
    void setNumerator(int n);
    void setDenomirator(int d);
 
+   //process
+   int plus(const fraction& f);
+   int minus(const fraction& f);
+   int multiple(const fraction& f);
+   int divide(const fraction& f);
+
+   //comparison
+   bool equal(const fraction& f);
+   bool notequal(const fraction& f);
+   bool less(const fraction& f);
+   bool more(const fraction& f);
+   bool lessequal(const fraction& f);
+   bool moreequal(const fraction& f);
+
    //output
    void output();
 
    
 };
+
+//implementation
 
 int fraction::GCD(int gc,int cd){
    gc=num;
@@ -45,7 +61,7 @@ int fraction::GCD(int gc,int cd){
    }
 return gc;
 }
-
+// check value that negative value is multipled with - 
 void fraction::simple(){
    if(de==0)return;
    if(de<0){
@@ -90,4 +106,8 @@ int fraction::getDenominator(){
    
 void fraction::output(){
     cout<<num<<"/"<<de;
+
+
+
+}
 }
--- source.cpp
+++ source.cpp
@@ -2,8 +2,11 @@
 #include"fraction.cpp"
 using namespace std;
 int main(){
-    fraction f1(6,9);
+    fraction f1(-6,9);
+    fraction f2(3,4);
     f1.output();
+    f2.output();
+    
 return 0;
 
 }
